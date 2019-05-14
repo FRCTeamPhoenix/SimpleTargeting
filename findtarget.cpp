b91d86f93bd367f4d3b125a7b71f8ff43de8f9ba
armedturret <rjwhitenh@outlook.com>
2019-05-14 09:33:59 -0400

    Increased the threshold to allow for only the reflective tape to show up

--- findtarget.cpp
+++ findtarget.cpp
@@ -1,16 +1,19 @@
 #include <opencv2/opencv.hpp>
 
 int main(){
-    cv::Mat img, img_canny;
+    cv::Mat img, img_processed, img_canny;
 
-    cv::VideoCapture input(1);
+	//run with camera input
+	cv::VideoCapture input(1);
+	//run with test footage
+    //cv::VideoCapture input("targetfootage.mp4");
     while(true){
         if(!input.read(img))
             break;
 
-        cv::threshold(img, img, 160, 255, CV_THRESH_BINARY_INV);
+        cv::threshold(img, img_processed, 240, 255, CV_THRESH_BINARY);
 
-        cv::Canny(img, img_canny, 75, 250, 3);
+        cv::Canny(img_processed, img_canny, 50, 200, 3);
 
         std::vector<std::vector<cv::Point> > contours;
         cv::findContours(img_canny, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
@@ -23,8 +26,9 @@ int main(){
                 //check if it has an area more than 100
                 if (cv::contourArea(shape) > 100)
                     cv::polylines(img, shape, true, cv::Scalar(0, 255, 0), 2, 8);
-                else
-                    cv::polylines(img, shape, true, cv::Scalar(0, 0, 255), 2, 8);
+                else{
+					//cv::polylines(img, shape, true, cv::Scalar(0, 0, 255), 2, 8);
+				}
                 
             }
         }
